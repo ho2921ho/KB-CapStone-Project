eb476a1b326d53f1d59f87f1b258191464a360f4
ho2921ho <ho2921ho@naver.com>
2020-03-08 16:51:08 +0900

    .

--- 2.feature_importance.py
+++ 2.feature_importance.py
@@ -2,12 +2,10 @@
 import os
 from os import listdir
 import pandas as pd
-import collections
 import numpy as np
 
 os.chdir("C:/DATA/KB_capstone")
 names = listdir("C:/DATA/KB_capstone/seperated data") # error point 폴더 안에 데이터만 있어야한다.
-names
 
 def csv(name):
     file = pd.read_csv('seperated data/' + name, engine = "python"); file.rename(columns = {"Unnamed: 0"  : "index"}, inplace = True) 
@@ -115,8 +113,11 @@ for i,df in enumerate(Vp):
     Vm.append(tmp) 
 
 ## 각 업종별 상관계수 선택. 
-
 coeflist = []
+
+def inter2rank(X):
+    for i in list(X):
+        X.loc[:,i] = X.loc[:,i].rank()
 
 def pear2all(X,y):
     features = X.columns.tolist() #variable name list
@@ -135,14 +136,17 @@ def pear2all(X,y):
     return data_correlations
 
 for index, df in enumerate(Vm):
+    print(index)
     y = pd.DataFrame(df["THSMON_SELNG_AMT"] / df["STOR_CO"], columns = ["SELNG_PRER_STOR"])
     y = np.sqrt(y)
     s = df.columns.get_loc("THSMON_SELNG_AMT") 
     e = df.columns.get_loc("AGRDE_60_ABOVE_SELNG_CO") 
     X = df.drop(columns = list(df)[s:e+1] ) # 예측의 대상이 되는 매출의 파생지표를 X에서 제거. 
     X = X.iloc[:,5:] # 상권명, 상권번호 등 y의 관련없는 변수 제거.
+    inter2rank(X)
     coef = pear2all(X,y)
     coeflist.append(coef)
+
 
 
 ## 10개 업종의 각 변수와의 평균 coef
@@ -161,16 +165,16 @@ mean_coef = sum_coef / 10
 
 tablelist = []
 for i, df_coef in enumerate(coeflist):
-    df_import = df_coef / mean_coef
+    df_import = df_coef - mean_coef ## 상대적 중요도 다시 정으;////
     table = df_coef.merge(df_import['Value'].to_frame(), how = 'left', on = df_coef.index)
-    table = table.rename({'key_0': indilist[i]+' variable_name','Value_x':'A_import','Value_y':'R_import'}, axis = 1)
+    table = table.rename({'key_0': 'variable_name','Value_x':indilist[i],'Value_y':'R_import'}, axis = 1)
     tablelist.append(table)        
 
-os.makedirs('table(Ysqrt_pearson)')
+os.makedirs('table(Ysqrt_sm_pearson)')
 
 ## save
 for i,df in enumerate(tablelist):
-    df.to_csv("C:/DATA/KB_capstone/table(Ysqrt_pearson)/" + indilist[i]+".csv",  encoding = "ms949")
+    df.to_csv("C:/DATA/KB_capstone/table(Ysqrt_sm_pearson)/" + indilist[i]+".csv",  encoding = "ms949")
 
 ## 매출 예측과 유사상권 정의를 위한 중요변수 fitering -> 상관계수의 절대값이 0.15이상인 변수만 사용.
 
@@ -202,5 +206,14 @@ for i,df in enumerate(Vsm):
     df.to_csv('C:/DATA/KB_capstone/filtered_data/'+indilist[i]+'_filterd.csv', encoding = "ms949", index =False)
 
 
-test = X.corr()
-test2= test.ix["MT_AVRG_INCOME_AMT"]
+# 변수 기준으로 tablelist 재정리.
+df0 = tablelist[0] 
+df0 = df0.iloc[:,:-1]
+
+for i in range(1,10):
+    df = tablelist[i]
+    df = df.iloc[:,:-1]
+    df0 = df0.merge(df, how = 'left', on = 'variable_name')
+
+os.makedirs('corr_table')
+df0.to_csv('C:/DATA/KB_capstone/corr_table/corr_table.csv')
